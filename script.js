76444890e0a00dda7cf6418286f45f430cca9134
raihan <maz.raihanz@gmail.com>
2024-02-26 18:38:05 +0700

    commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('toggleFlame').addEventListener('click', function() {
+    this.classList.toggle('off');
+    document.getElementById('texttoggleFlame').classList.toggle('on');
+});
